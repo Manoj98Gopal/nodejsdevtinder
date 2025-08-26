2d589d797efe51f3fa61bc2e99bb27cedd519cb4
manoj98gopal <manoj98gopal@gmail.com>
2025-08-26 22:36:40 +0530

    error handling pushed

--- src/app.js
+++ src/app.js
@@ -1,39 +1,31 @@
 const exp = require("express");
-const AdminMiddleware = require("./middlewares/admin");
-const UserMiddleware = require("./middlewares/userMiddleware");
 
 const app = exp();
 
 const PORT = 7777;
 
-app.use("/admin", AdminMiddleware);
-
-app.get("/admin/getUser", (req, res) => {
-  res.send("Sending all users data");
+app.get("/user", (req, res) => {
+  try {
+    throw new Error("User not found");
+    res.send("User Route");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
-app.delete("/admin/deleteUser", (req, res) => {
-  res.send("Deleting user data");
+app.get("/admin", (req, res) => {
+  throw new Error("Admin not found");
+  res.send("Admin Route");
 });
 
-app.get("/users", UserMiddleware, (req, res) => {
-  res.send("Sending all users data");
-});
-
-app.post("/users", UserMiddleware, (req, res) => {
-  res.send("Creating a new user");
-});
-
-app.put("/users", UserMiddleware, (req, res) => {
-  res.send(`Updating user with ID`);
-});
-
-app.delete("/users", UserMiddleware, (req, res) => {
-  res.send(`Deleting user with ID`);
-});
-
-app.post("/users/login", (req, res) => {
-  res.send(`user login api`);
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    console.error(err.stack);
+    res.status(500).send("Something broke!");
+  } else {
+    next();
+  }
 });
 
 app.listen(PORT, () => {
