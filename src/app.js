7479eaebd719b2f81dd41429a3a7278fa55c43df
manoj98gopal <manoj98gopal@gmail.com>
2025-08-29 22:40:16 +0530

    crud operation complited

--- src/app.js
+++ src/app.js
@@ -6,23 +6,59 @@ const app = exp();
 
 const PORT = 7777;
 
+app.use(exp.json());
+
 app.post("/signup", async (req, res) => {
-  const userObj = {
-    firstName: "Prem",
-    lastName: "Kumar",
-    emailId: "prem@example.com",
-    password: "paremord123",
-    age: 45,
-    gender: "Male"
-  };
-
-  const user = new User(userObj);
-
+  const user = new User(req.body);
   try {
     await user.save();
     res.send("User signed up successfully");
   } catch (error) {
     console.error("Error signing up user:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/getUsers", async (req, res) => {
+  try {
+    const results = await User.find({});
+    res.send(results);
+  } catch (error) {
+    console.error("unable to fetch users", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/getUsers/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const results = await User.findById(id);
+    res.send(results);
+  } catch (error) {
+    console.error("unable to fetch users", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.delete("/deleteUser/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log("id :", id);
+    await User.findByIdAndDelete(id);
+    res.send("Successfully deleted..!");
+  } catch (error) {
+    console.error("unable to fetch users", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.patch("/updateUser", async (req, res) => {
+  try {
+    const tempData = req.body;
+    const result = await User.findByIdAndUpdate(tempData?.id, tempData,{returnDocument:"after"});
+    res.send(result);
+  } catch (error) {
+    console.error("unable to fetch users", error);
     res.status(500).send("Internal Server Error");
   }
 });
