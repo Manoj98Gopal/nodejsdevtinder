3541022d19c64bab32737de184328c9d526e7c44
manoj98gopal <manoj98gopal@gmail.com>
2025-09-21 16:05:46 +0530

    react router dom installed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
-import { Button } from "./components/ui/button";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Body from "./components/common/Body";
+import Feed from "./pages/Feed";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Connections from "./pages/Connections";
+import Requests from "./pages/Requests";
 
 function App() {
   return (
-    <>
-      <h1>Hello working fine</h1>
-      <Button>Working fine</Button>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Body />}>
+          <Route index element={<Feed />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/connections" element={<Connections />} />
+          <Route path="/requests" element={<Requests />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
