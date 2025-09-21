fcb5f61e563984942752c0afe5922ae358fbbaf4
manoj98gopal <manoj98gopal@gmail.com>
2025-09-21 15:03:39 +0530

    fornted project installed with shadcn

---
+++ frontend/README.md
@@ -0,0 +1,2 @@
+# watch for installing react js with shadcn 
+  https://youtu.be/aMX_DYK5LAk?si=PnR7j-B-uOV5gL1K
---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>Hello working fine</h1>
+      <Button>Working fine</Button>
+    </>
+  );
+}
+
+export default App;
